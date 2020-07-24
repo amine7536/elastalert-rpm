d8a83f64dcc46b9c92cbd7ed9bb916c4447a1fb9
Thomas Andrejak <thomas.andrejak@gmail.com>
2020-07-24 09:58:33 +0200

    Bump to Elastalert 0.2.4

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@ set -xe
 
 export BASEDIR=$(dirname "$0")
 export NAME="elastalert"
-export VERSION="0.1.18"
+export VERSION="0.2.4"
 export REVISION="1.el7"
 export BUILDDIR="/tmp/build"
 export INSTALLDIR="/usr/share/python"
 
 yum -y install ruby ruby-devel gcc make rpm-build openssl-devel libffi-devel epel-release
-yum -y install python-pip  python-virtualenv
-pip install virtualenv-tools
-pip install --upgrade pip
+yum -y install python3-pip  python3-virtualenv
+pip3 install virtualenv-tools
+pip3 install --upgrade pip
 gem install fpm --no-doc
 
 rm -fr $BUILDDIR
@@ -28,15 +28,15 @@ mkdir -p $BUILDDIR/usr/lib/systemd/system
 cp $BASEDIR/conf/elastalert.service $BUILDDIR/usr/lib/systemd/system
 
 # Virtual Env
-virtualenv $BUILDDIR$INSTALLDIR/elastalert
-$BUILDDIR$INSTALLDIR/elastalert/bin/pip install --upgrade pip
-$BUILDDIR$INSTALLDIR/elastalert/bin/pip install -r $BASEDIR/requirements.txt
-$BUILDDIR$INSTALLDIR/elastalert/bin/pip install "elastalert==$VERSION"
+virtualenv-3 $BUILDDIR$INSTALLDIR/elastalert
+$BUILDDIR$INSTALLDIR/elastalert/bin/pip3 install --upgrade pip
+$BUILDDIR$INSTALLDIR/elastalert/bin/pip3 install "elasticsearch==7.5.1"
+$BUILDDIR$INSTALLDIR/elastalert/bin/pip3 install "elastalert==$VERSION"
 
 find $BUILDDIR ! -perm -a+r -exec chmod a+r {} \;
 
 cd $BUILDDIR$INSTALLDIR/elastalert
- virtualenv-tools --update-path $INSTALLDIR/elastalert
+ virtualenv-tools3 --update-path $INSTALLDIR/elastalert
 cd -
 
 # Clean up
